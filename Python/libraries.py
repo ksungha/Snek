fbc14996841c182351cdd322c12fd6b5794cd691
Sung Kim <ksungha66@gmail.com>
2022-06-22 12:41:02 -0700

    Adding a libraries files

---
+++ Python/libraries.py
@@ -0,0 +1,16 @@
+import pygame
+import time
+import random
+
+snake_speed = 15
+
+# Window size
+win_x = 800
+win_y = 800
+
+# defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
